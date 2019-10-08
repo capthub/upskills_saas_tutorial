83aca3f1da5b4f1cb09331512fec54b9c4ee793e
Hubert Sieron <hubert.sieron@gmail.com>
2019-10-08 12:05:06 +0000

    Added mailer settings to environment file

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'hubert.sieron@gmail.com'
+    default to: 'czelolada@gmail.com'
     
     def contact_email(name, email, body)
        @name= name
