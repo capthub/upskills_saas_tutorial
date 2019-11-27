c703d4e886e37850e2a5b7ad5cab0376008977fb
Hubert Sieron <hubert.sieron@gmail.com>
2019-11-27 15:28:53 +0000

    Added premium content and updated about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
+    before_action :authenticate_user!, only: [:pro]
     def home 
         @basic_plan = Plan.find(1)
         @pro_plan = Plan.find(2)
     end
     
     def about
+    end
+    def pro
     end
 end
